8eba04e847707a40de96e175ab31a38863824a11
Bibiax19 <167220042+Bibias19@users.noreply.github.com>
2024-11-06 17:17:12 -0300

    java.commit

---
+++ src/javascript/script.js
@@ -0,0 +1,5 @@
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const recipeslist = document.getElementById('recipeslist');
+const modalContainer = document.getElementById('modalContainer');
+const recipesdetailsContent = document.getElementById('recipes-details-Content');
